a6b0ec267251a0c8ff629c7420f60aaeb50c2302
HydrusSystemsMx <icerna@minsait.com>
2021-09-22 16:05:30 -0500

    Add last changes

---
+++ src/main/java/com/icn/barleystation/components/Scheduler.java
@@ -0,0 +1,19 @@
+package com.icn.barleystation.components;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+	@Scheduled(fixedRate = 45000)
+	public void fixedRateSch() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+		Date now = new Date();
+		String strDate = sdf.format(now);
+		System.out.println("Fixed Rate scheduler:: " + strDate);
+	}
+}
